942f1679f6e5145e596a7f3f76f36931eeaacf95
adityadubey2211-prog <adityadubey2211@gmail.com>
2026-07-30 01:51:11 +0530

    buid basic interface

---
+++ index.js
@@ -0,0 +1,24 @@
+const type = document.querySelector("#typing");
+const text = "Aditya Dubey";
+type.textContent = "";
+let idx = 0;
+function typer(){
+    console.log("done")
+    if(idx < text.length){
+        type.innerHTML += text.charAt(idx);
+        idx++;
+        setTimeout(typer, 250);
+    }
+}
+typer();
+const stars = document.getElementById("stars");
+
+for(let i=0;i<550;i++){
+    const star=document.createElement("div");
+    star.classList.add("star");
+    star.style.left=Math.random()*100+"%";
+    star.style.top=Math.random()*100+"%";
+    star.style.animationDelay=Math.random()*3+"s";
+    star.style.opacity=Math.random();
+    stars.appendChild(star);
+}
